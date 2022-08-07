26f62ee543724015df456fb3a14c784fa1136eda
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-08-07 22:32:29 +0530

    Compute a+b*c

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -8,3 +8,15 @@ read num3
 echo $num1
 echo $num2
 echo $num3
+
+
+echo "*******UC1********"
+echo "We are computing a + b * c"
+echo "a = 10"
+echo "b = 25"
+echo "c = 30"
+a=10
+b=25
+c=30
+val=$((a + b * c))
+echo $val
